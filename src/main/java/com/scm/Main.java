c112081084caa583821b835fb4aef5037a2ab75f
jishnupumex <139556055+jishnupumex@users.noreply.github.com>
2023-10-30 17:51:58 +0530

    Initial commit

---
+++ src/main/java/com/scm/Main.java
@@ -0,0 +1,7 @@
+package com.scm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Supply Chain common library");
+    }
+}
---
+++ src/main/java/com/scm/UserOrder.java
@@ -0,0 +1,106 @@
+package com.scm;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class UserOrder implements Serializable {
+    @JsonProperty
+    private int userId;
+    @JsonProperty
+    private int prodId;
+    @JsonProperty
+    private String prodName;
+    @JsonProperty
+    private String prodDesc;
+    @JsonProperty
+    private String prodType;
+    @JsonProperty
+    private double prodPrice;
+    @JsonProperty
+    private int prodQty;
+    @JsonProperty
+    private double totalPrice;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public String getProdDesc() {
+        return prodDesc;
+    }
+
+    public void setProdDesc(String prodDesc) {
+        this.prodDesc = prodDesc;
+    }
+
+    public String getProdType() {
+        return prodType;
+    }
+
+    public void setProdType(String prodType) {
+        this.prodType = prodType;
+    }
+
+    public double getProdPrice() {
+        return prodPrice;
+    }
+
+    public void setProdPrice(double prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+
+    public int getProdQty() {
+        return prodQty;
+    }
+
+    public void setProdQty(int prodQty) {
+        this.prodQty = prodQty;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "UserOrder{" +
+                "userId=" + userId +
+                ", prodId=" + prodId +
+                ", prodName='" + prodName + '\'' +
+                ", prodDesc='" + prodDesc + '\'' +
+                ", prodType='" + prodType + '\'' +
+                ", prodPrice=" + prodPrice +
+                ", prodQty=" + prodQty +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
